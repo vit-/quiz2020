4fd0fcd487bb783bd07ced76c97092bc23523f56
Vitalii Vokhmin <vitaliy.vokhmin@gmail.com>
2019-12-29 11:42:04 +0200

    Initial impl

---
+++ app.js
@@ -0,0 +1,64 @@
+const transitionTime = 100;
+
+$(document).ready(function () {
+    $('#loading').hide(transitionTime, function () {
+        $('#welcome').show(transitionTime);
+    });
+});
+
+$('.start').click(function () {
+    $('#welcome').hide(transitionTime, function () {
+        $('#game').show(transitionTime);
+        tickGame();
+    });
+});
+
+$('#next .btn-primary').click(tickGame);
+
+$(document).on('click', '#game .btn.correct', function () {
+    console.log('Correct!');
+    $('#result .alert-danger').hide(transitionTime);
+    $('#result .alert-success').show(transitionTime);
+    $('#task').hide(transitionTime);
+    $('#next').show(transitionTime);
+});
+
+$(document).on('click', '#game .btn.incorrect', function (event) {
+    console.log('Incorrect!');
+    $(event.target).addClass('btn-danger');
+    $('#result .alert-danger').show(transitionTime);
+    $('#result .alert-success').hide(transitionTime);
+});
+
+function endGame() {
+    $('#game').hide(transitionTime, function () {
+        $('#finish').show(transitionTime);
+    });
+}
+
+function tickGame() {
+    let question = questions.shift();
+    if (question === undefined) {
+        // game finished
+        endGame()
+    } else {
+        $('#result .alert').hide();
+        $('#next').hide();
+        $('#explanation').hide().text(question.explanation);
+        $('#task').hide().empty().append($('<div class="row mx-auto"></div>').append($('<h1></h1>').text(question.text)));
+        question.answers.forEach(function(answer, index) {
+            $('#task').append(renderAnswer(answer));
+        });
+        $('#task').show(transitionTime);
+    }
+}
+
+function renderAnswer(answer) {
+    let btn = $('<button class="btn btn-secondary w-100 text-center"></button>').text(answer.text);
+    if (answer.isCorrect) {
+        btn.addClass('correct');
+    } else {
+        btn.addClass('incorrect');
+    }
+    return $('<div class="row mx-auto"></div>').append(btn);
+}
---
+++ questions.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+        text: "Q1",
+        explanation: "it is what it is",
+        answers: [
+            {text: 'A1', isCorrect: false},
+            {text: 'A1', isCorrect: false},
+            {text: 'A1', isCorrect: false},
+            {text: 'A2', isCorrect: true},
+        ]
+    },
+    {
+        text: "Q2",
+        explanation: "YAY",
+        answers: [
+            {text: 'A1', isCorrect: false},
+            {text: 'A2', isCorrect: true},
+        ]
+    },
+];
